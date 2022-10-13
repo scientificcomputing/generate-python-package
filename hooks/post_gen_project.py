c0f76189782b5f5f55cdaae6e8aefd962c5d5bf0
jorgensd <dokken92@gmail.com>
2022-10-13 13:43:43 +0200

    Add coverage and use options for testing to avoid duplicate tests

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,18 +2,26 @@
 import os
 
 REMOVE_PATHS = [
-    '{% if cookiecutter.testing|lower != "y" %} tests/test_version.py{% endif %}',
-    '{% if cookiecutter.testing|lower != "y" %} tests/{% endif %}',
-    '{% if cookiecutter.testing|lower != "y" %} .github/workflows/test_package.yml{% endif %}',
-    '{% if cookiecutter.linting|lower != "y" %} .github/workflows/check_formatting.yml{% endif %}',
-    '{% if cookiecutter.linting|lower != "y" %} .flake8{% endif %}',
-    '{% if cookiecutter.build_docs|lower != "y" %} .github/workflows/build_docs.yml{% endif %}',
-    '{% if cookiecutter.build_docs|lower != "y" %} docs/_config.yml{% endif %}',
-    '{% if cookiecutter.build_docs|lower != "y" %} docs/_toc.yml{% endif %}',
-    '{% if cookiecutter.build_docs|lower != "y" %} docs/api.rst{% endif %}',
-    '{% if cookiecutter.build_docs|lower != "y" %} docs/index.md{% endif %}',
-    '{% if cookiecutter.build_docs|lower != "y" %} docs/logo.png{% endif %}',
-    '{% if cookiecutter.build_docs|lower != "y" %} docs{% endif %}'
+    {%- if cookiecutter.testing|lower == "none" %}
+    'tests/test_version.py',
+    'tests/',
+    '.github/workflows/test_package.yml',
+    {%- elif cookiecutter.testing|lower == "pytest" %}
+    '.github/workflows/test_package_coverage.yml',
+    {%- elif cookiecutter.testing|lower == "pytest-cov" %}
+    '.github/workflows/test_package.yml',
+    {%- endif %}
+    {%- if cookiecutter.linting|lower != "y" %}
+    '.github/workflows/check_formatting.yml',
+    '.flake8',
+    '.github/workflows/build_docs.yml',
+    'docs/_config.yml',
+    'docs/_toc.yml',
+    'docs/api.rst',
+    'docs/index.md',
+    'docs/logo.png',
+    'docs'
+    {% endif %}
 ]
 
 for path in REMOVE_PATHS:
