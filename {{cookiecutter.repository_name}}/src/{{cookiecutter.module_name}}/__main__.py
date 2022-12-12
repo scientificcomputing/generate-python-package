e23ecc8427bf043b573c05aefe6eb36652eb1262
Henrik Finsberg <henrikfinsberg@hotmail.com>
2022-12-12 09:29:17 +0100

    Add typer as cli option (#19)

--- {{cookiecutter.repository_name}}/src/{{cookiecutter.module_name}}/__main__.py
+++ {{cookiecutter.repository_name}}/src/{{cookiecutter.module_name}}/__main__.py
@@ -3,6 +3,11 @@
 from {{cookiecutter.module_name}}.cli import main
 {% if cookiecutter.command_line_interface|lower != 'none' -%}
 if __name__ == "__main__":
+    {% if cookiecutter.command_line_interface|lower == 'typer' -%}
+    import typer
+    typer.run(main)
+    {% else -%}
     import sys
     sys.exit(main())
+    {% endif -%}
 {% endif -%}
